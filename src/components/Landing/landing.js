0a299af3ae8b5d6d711bc40cb014e66df5979c4e
haitv <haitv@sunshinegroup.vn>
2020-03-25 17:08:11 +0700

    [LANDING] Style for landing page

--- src/components/Landing/landing.js
+++ src/components/Landing/landing.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import './landing.css';
 
 export const Landing = () => (
-        <div className="root-content">
-            
+        <div className="landing-container">
+                <div className="circle-item"></div>
+                <div className="ellip-item">
+                </div>
+                <button className="btn-shop-now">SHOP NOW</button>
         </div>
 )
