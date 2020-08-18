5bddc5c5da7c0797adc2915afc61b5c0a9456e56
haitv <haitv@sunshinegroup.vn>
2020-08-18 16:53:32 +0700

    update code

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -26,7 +26,7 @@ const Contact = (props) => {
 
     return (
         width > 800 && height > 600 ?
-        <Col md={{ span: 24 }} xs={{ span: 8 }} sm={{ span: 16 }} style={{ padding: '8px' }}>
+        <Col md={{ span: 24 }} xs={{ span: 8 }} sm={{ span: 16 }}>
             <Row gutter={{ xs: 7, sm: 16, md: 24, lg: 32 }} style={{ marginTop: '20px', backgroundColor: '#000' }}    >
                 <Col xs={{ span: 12 }} style={{ backgroundColor: 'rgba(250, 246, 246, 0.055)', color: '#fff' }}>
                     <Col span={24}>contact us</Col>
@@ -36,7 +36,7 @@ const Contact = (props) => {
                     <Col span={24}><FacebookOutlined title='Facebook' style={{ backgroundColor: '#2943c4', fontSize: '18px' }} /></Col>
                     <Col span={24}><InstagramOutlined title='Instagram' style={{ backgroundColor: '#4f29c2', fontSize: '18px' }} /></Col>
                 </Col>
-                <Col span={12} style={{ height: '500px', padding: 0 }}>
+                <Col span={12} style={{ height: '500px' }}>
                     <img src={props.cover} alt="" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
                 </Col>
             </Row>
@@ -49,7 +49,7 @@ const Contact = (props) => {
                 <Col span={8} ></Col>
             </Row>
         </Col> :
-        <Col md={{ span: 24 }} xs={{ span: 12 }} sm={{ span: 16 }} style={{ padding: '8px' }}>
+        <Col md={{ span: 24 }} xs={{ span: 12 }} sm={{ span: 16 }}>
             <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} style={{ marginTop: '20px', backgroundColor: '#000' }}    >
                 <Col xs={{ span: 10 }} style={{ backgroundColor: 'rgba(250, 246, 246, 0.055)', color: '#fff' }}>
                     <Col span={24}>contact us</Col>
@@ -59,7 +59,7 @@ const Contact = (props) => {
                     <Col span={24}><FacebookOutlined title='Facebook' style={{ backgroundColor: '#2943c4', fontSize: '18px' }} /></Col>
                     <Col span={24}><InstagramOutlined title='Instagram' style={{ backgroundColor: '#4f29c2', fontSize: '18px' }} /></Col>
                 </Col>
-                <Col span={12} style={{ height: '500px', padding: 0 }}>
+                <Col span={12} style={{ height: '500px' }}>
                     <img src={props.cover} alt="" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
                 </Col>
             </Row>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './footer.css';
 import { Chat } from '../Chat';
-import useDimensions from '../../helper/feature'
+import useDimensions from '../../utils/dimensions'
 export const Footer = () => {
     const { width, height } = useDimensions();
     return (
