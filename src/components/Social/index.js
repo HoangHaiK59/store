e0dfe511df58787dff34f5d0fae6af51aaf40fa1
haitv <haitv@sunshinegroup.vn>
2020-09-10 17:11:22 +0700

    Fix loi position

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './social.css';
+import { message } from 'antd';
 import { FacebookFilled, InstagramFilled, TwitterCircleFilled } from '@ant-design/icons';
 export default function Social(props) {
+    const connectSocial = () => {
+        message.info('Connect social network')
+    }
     return (
         <div className="social-container">
-            <FacebookFilled  size={25} style={{fontSize: '25px', color:"#fa520a"}}/>
-            <InstagramFilled color="#717feb" size={25} style={{fontSize: '25px', marginTop: '15px',color:"#fa520a"}}/>
-            <TwitterCircleFilled color="#717feb" size={25} style={{fontSize: '25px', marginTop: '15px', color:"#fa520a"}}/>
+            <FacebookFilled  size={25} style={{fontSize: '25px', color:"#fa520a"}} onClick={connectSocial}/>
+            <InstagramFilled color="#717feb" size={25} style={{fontSize: '25px', marginTop: '15px',color:"#fa520a"}} onClick={connectSocial}/>
+            <TwitterCircleFilled color="#717feb" size={25} style={{fontSize: '25px', marginTop: '15px', color:"#fa520a"}} onClick={connectSocial}/>
         </div>
     )
 }
