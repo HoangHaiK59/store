0f0703d21da614e249b3b97a13a57d274b831afe
haitv <trinhhaik59@gmail.com>
2020-09-14 20:32:45 +0700

    update code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './home.css';
-import { Row, Col, Carousel, Breadcrumb, Spin } from 'antd';
+import { Spin } from 'antd';
 import { connect } from 'react-redux';
-import { storeActions } from '../../store/actions/store.action';
 import { instance } from '../../utils/axios';
 import Content from '../Content';
 
@@ -65,79 +64,6 @@ class Home extends React.Component {
         }
     }
 
-
-    //     <Col span={12} >
-    //     <div className="content-header__description">
-    //         <div className="content-header__description--cover">
-    //             <h2>Style us</h2>
-    //         </div>
-    //         <button className="content-header__description--checkoutbtn">
-    //         </button>
-    //     </div>
-    // </Col>
-
-    //     <Col md={{span: 24}} sm={{span: 16}} xs={{span: 16}} >
-    //     <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify="start">
-    //         <Col md={{span: 12}} sm={{span: 8}} xs={{span: 10}} >
-    //             <h5>Collection 2</h5>
-    //         </Col>
-    //         <Col md={{span: 12}} sm={{span: 8}} xs={{span: 8}} style={{ textAlign: 'right' }}>
-    //             <h5>View all</h5>
-    //         </Col>
-    //     </Row>
-    // </Col>
-    // <Col md={{span: 24}} sm={{span: 16}} xs={{span: 16}}>
-    //     <div className='items-container' style={{ marginTop: '10px' }}>
-    //         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify="start" align="middle" style={{ width: '95%' }}>
-    //             {
-    //                 this.state.products.map((product, id) => <Col onClick={() => this.handleClick(product.id)} 
-    //                 key={id}
-    //                 xs={{span: 16}}
-    //                 sm={{span: 16}}
-    //                 md={{span: 3}}
-    //                  className="product">
-    //                     <div className="product-header">
-    //                         <div className="product-cover">
-    //                             <img src={product.image} alt="productImage" />
-    //                         </div>
-    //                     </div>
-    //                     <h5 className="product-name">{product.name}</h5>
-    //                 </Col>)
-    //             }
-    //         </Row>
-    //     </div>
-    // </Col>
-    // <Col md={{span: 24}} sm={{span: 16}} xs={{span: 16}}>
-    //     <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify="start">
-    //         <Col md={{span: 12}} sm={{span: 8}} xs={{span: 10}}>
-    //             <h5>Collection 3</h5>
-    //         </Col>
-    //         <Col md={{span: 12}} sm={{span: 8}} xs={{span: 8}} style={{ textAlign: 'right' }}>
-    //             <h5>View all</h5>
-    //         </Col>
-    //     </Row>
-    // </Col>
-    // <Col md={{span: 24}} sm={{span: 16}} xs={{span: 16}} >
-    //     <div className='items-container' style={{ marginTop: '10px' }}>
-    //         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify="start" align="middle" style={{ width: '95%' }}>
-    //             {
-    //                 this.state.products.map((product, id) => <Col onClick={() => this.handleClick(product.id)}
-    //                  key={id}
-    //                  xs={{span: 16}}
-    //                  sm={{span: 16}}
-    //                  md={{span: 3}}
-    //                  className="product">
-    //                     <div className="product-header">
-    //                         <div className="product-cover">
-    //                             <img src={product.image} alt="productImage" />
-    //                         </div>
-    //                     </div>
-    //                     <h5 className="product-name">{product.name}</h5>
-    //                 </Col>)
-    //             }
-    //         </Row>
-    //     </div>
-    // </Col>
     render() {
         return (
             this.state.products.length > 0 ? <Content items={this.state.products}  handleClick={this.handleClick.bind(this)}/>:
